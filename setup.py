e5f763cf459ef4c186bc88cd80848c1d71e98ae5
SNL-NERL <41639617+SNL-NERL@users.noreply.github.com>
2018-10-02 14:36:21 -0600

    Root directory changes for 0.9.2

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
-# Copyright 2018 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
-# Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights in this software
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation version 3 of the License only.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='whetstone', version='0.8.8', description='Provides extensions to keras to allow training deep spiking neural networks',packages=find_packages())
+setup(name='whetstone', version='0.9.2', description='Provides extensions to keras to allow training deep spiking neural networks',packages=find_packages(exclude=("Release",)))
